cdcc3daabcdba1ceb66b9ead01f3277dab692996
IsaiasEspinal <isaiasespinal19@gmail.com>
2023-11-04 19:55:22 -0400

    sj

--- class/views/Text.js
+++ class/views/Text.js
@@ -1,15 +1,16 @@
 class Text{
-    constructor(text,x,y,fontSize){
+    constructor(text,color="#fff",x = null,y = null ,fontSize){
         this.text = text;
+        this.color = color;
         this.fontSize = fontSize;
-        this.x = x;
-        this.y = y;
+        this.x = x || (app.width/2 - this.textlength/2);
+        this.y = y || (app.height/2);
 
         this.textlength = fontSize* text.length;
     }
 
     render(ctx){
-        ctx.fillStyle = "#fff";
+        ctx.fillStyle = this.color;
         ctx.font = `${this.fontSize}pt Arial`
         ctx.fillText(this.text, this.x, this.y);
     }
