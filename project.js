b221bf03014aacb01de37aea6b074040c4f13ab1
JohnnyelPerdomo <johnnyelperdomo@gmail.com>
2023-11-04 18:19:29 -0400

    se agrego metodos de personaje y se creo la clase enemigo

--- project.js
+++ project.js
@@ -1,16 +1,34 @@
 
-class personaje{
+class Personaje{
 
-    constructor(nombre,vida) {
+    constructor(nombre,vida,tipo) {
         this.nombre = nombre;
         this.vida = vida;
+        this.tipo=tipo;
+      }
+
+      atacar(enemigo) {
+        console.log(this.nombre, "Ataca a ",+ enemigo.name);
+        enemigo.recibirAtaque();
+      }
+
+      recibirAtaque(ataqueDano) {
+        this.vida -= ataqueDano;
+        console.log(`${this.nombre} ha recibido un ataque. Vida restante: ${this.vida}`);
+        if (this.vida <= 0) {
+          this.morir();
+        }
       }
 
 morir(){
-console.log("Has Muerto"); 
+   console.log("Has Muerto"); 
 }
 
+
 }
 
+class enemigo extends Personaje{
 
+
+}
 
