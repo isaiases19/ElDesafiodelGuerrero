61562818e867a38608d6af1e34aa5a18960b8467
IsaiasEspinal <isaiasespinal19@gmail.com>
2023-11-04 19:36:08 -0400

    init

--- class/views/Text.js
+++ class/views/Text.js
@@ -1,6 +1,16 @@
 class Text{
-    constructor(text){
+    constructor(text,x,y,fontSize){
         this.text = text;
+        this.fontSize = fontSize;
+        this.x = x;
+        this.y = y;
+
+        this.textlength = fontSize* text.length;
     }
 
+    render(ctx){
+        ctx.fillStyle = "#fff";
+        ctx.font = `${this.fontSize}pt Arial`
+        ctx.fillText(this.text, this.x, this.y);
+    }
 }
---
+++ prueba.js
@@ -0,0 +1,7 @@
+function main(){
+
+
+
+}
+//inicializacion
+main();
