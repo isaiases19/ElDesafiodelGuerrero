428d00a6de68a41824f246f0fce4008397353853
IsaiasEspinal <isaiasespinal19@gmail.com>
2023-11-17 23:30:27 -0500

    enemy spawn position

--- utility/utility.js
+++ utility/utility.js
@@ -1,30 +1,30 @@
 import { app } from "../main.js";
 import { Enemigo } from "../class/Enemigo.js";
-function randomMinMax(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomMinMax(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 //delay funcion
 async function delay(ms) {
-    return new Promise(resolve => {
-       app.timeOut = setTimeout(resolve, ms);
-    });
+  return new Promise(resolve => {
+    app.timeOut = setTimeout(resolve, ms);
+  });
 }
 
-function generadorEnemigo(x = 750){
+function generadorEnemigo(x = 750) {
 
 
-  
-  let level=0;
-  app.player.nivel>10?level=randomMinMax(8,15) :level=randomMinMax(1,5);
-  
-    app.enemigos.push(new Enemigo("Troll", "enemy",level*20 ,level+0.5 ,level+1,level,{x}));
-  }
 
-  function calcularDistancia(x1, y1, x2, y2) {
-    return Math.hypot(x2 - x1, y2 - y1);
-  }
-  function calcularDistancia1D(x1, x2,) {
-    return Math.hypot(x2,x1);
-  }
+  let level = 0;
+  app.player.nivel > 10 ? level = randomMinMax(8, 15) : level = randomMinMax(1, 5);
+  x = randomMinMax(1,10) <= 5 ? app.width: -100; 
+  app.enemigos.push(new Enemigo("Troll", "enemy", level * 20, level + 0.5, level + 1, level, { x }));
+}
 
-export {randomMinMax,delay,generadorEnemigo,calcularDistancia,calcularDistancia1D}
+function calcularDistancia(x1, y1, x2, y2) {
+  return Math.hypot(x2 - x1, y2 - y1);
+}
+function calcularDistancia1D(x1, x2,) {
+  return Math.hypot(x2, x1);
+}
+
+export { randomMinMax, delay, generadorEnemigo, calcularDistancia, calcularDistancia1D }
