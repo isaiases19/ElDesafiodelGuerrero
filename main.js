50e068f9c688c4ba864b52e91b148ea1869b5479
IsaiasEspinal <isaiasespinal19@gmail.com>
2023-11-19 13:13:48 -0500

    localStorage

--- main.js
+++ main.js
@@ -40,13 +40,15 @@ async function setup() {
     //configura musica
     app.music.loop = true;
     app.music.play()
+    const musicMuted = localStorage.getItem("musicMuted");
+    app.music.muted = musicMuted;
     const song = [0.16,1.55,3.19,3.45,4.51,5.50];
     app.music.currentTime = song[randomMinMax(1,song.length) - 1] * 60;
     app.music.volume = 0.3;
     //genera enemigo
     generadorEnemigo();
     //crea player
-    app.player = new Guerrero("Conan","player",120,10,7,20);
+    app.player = new Guerrero("Conan","player",120,10,7,1);
     
     await delay(1000);
     draw()
